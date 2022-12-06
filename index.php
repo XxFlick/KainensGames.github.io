b17fb3d95b5f2a6730d580132367c3c925c84c95
XxFlick <52386985+XxFlick@users.noreply.github.com>
2022-12-06 13:51:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="Styless.css">
+    <link rel="icon" type="image/png" href="fav.png" sizes="96x96">
+    <title>Kainen's Games</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300&display=swap" rel="stylesheet">
+                <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-215053213-1">
+    </script>
+    
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-215053213-1');
+    </script>
+</head>
+<body>
+    <header class="header">
+        <div class="headerbg">
+            <a href="https://kainensgames.com" class="headertext">
+                <img class="logo" src="fav.png">
+                <p>Kainen's Games</p>
+            </a>
+			<div class="SearchContainer"><input class="search" placeholder="Search..." type="text"></div>
+			<li class="headerlist">
+				<a class="headerlistlink" href="Games.html">
+					<img class="headerimgs" src="home.svg">
+				</a>
+				<a class="headerlistlink" href="Games.html">
+					<img class="headerimgs" src="profile.svg">
+				</a>
+			</li>
+			
+        </div>
+    
+    </header>
+<div class="GamesContainer">
+	<div class="MostPlayed">
+		<div class="categorytext">Hot Games</div>
+		<div class="moretext"><a>More...</a></div>
+		<div class="owl-carousel owl-theme">
+    <div class="item1">
+        <a href="https://kainensgames.com/RetroBowl">
+            <img class="gameimg" src="Retro.jpg">
+            <div class="hovershow1">
+                <div class="title">
+                    Retro Bowl
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="item2">
+        <img class="gameimg" src="moto1.jpg">
+        <div class="hovershow2">
+            <div class="title">
+                Moto X3M
+            </div>
+        </div>
+    </div>
+	<div class="item3"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item4"><img class="gameimg" src="Retro.jpg"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="Retro.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	
+	</div>
+	</div>
+
+<div class="RecentlyPlayed">
+		<div class="categorytext">Recently Played</div>
+        <div class="moretext"><a>More...</a></div>
+		<div class="owl-carousel owl-theme">
+    <div class="item1">
+        <a href="https://kainensgames.com/RetroBowl">
+            <img class="gameimg" src="Retro.jpg">
+            <div class="hovershow1">
+                <div class="title">
+                    Retro Bowl
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="item2">
+        <img class="gameimg" src="moto1.jpg">
+        <div class="hovershow2">
+            <div class="title">
+                Moto X3M
+            </div>
+        </div>
+    </div>
+	<div class="item3"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item4"><img class="gameimg" src="Retro.jpg"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="Retro.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	
+	</div>
+	</div>
+
+<div class="Recommended">
+		<div class="categorytext">Reccomended</div>
+        <div class="moretext"><a>More...</a></div>
+		<div class="owl-carousel owl-theme">
+    <div class="item1">
+        <a href="https://kainensgames.com/RetroBowl">
+            <img class="gameimg" src="Retro.jpg">
+            <div class="hovershow1">
+                <div class="title">
+                    Retro Bowl
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="item2">
+        <img class="gameimg" src="moto1.jpg">
+        <div class="hovershow2">
+            <div class="title">
+                Moto X3M
+            </div>
+        </div>
+    </div>
+	<div class="item3"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item4"><img class="gameimg" src="Retro.jpg"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="Retro.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	
+	</div>
+	</div>
+
+<div class="Browse">
+		<div class="categorytext">Browse</div>
+        <div class="moretext"><a>More...</a></div>
+		<div class="owl-carousel owl-theme">
+    <div class="item1">
+        <a href="https://kainensgames.com/RetroBowl">
+            <img class="gameimg" src="Retro.jpg">
+            <div class="hovershow1">
+                <div class="title">
+                    Retro Bowl
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="item2">
+        <img class="gameimg" src="moto1.jpg">
+        <div class="hovershow2">
+            <div class="title">
+                Moto X3M
+            </div>
+        </div>
+    </div>
+	<div class="item3"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item4"><img class="gameimg" src="Retro.jpg"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="moto1.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="Retro.jpg"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	<div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+    <div class="item"><img class="gameimg" src="default-placeholder.png"></div>
+	
+	</div>
+	</div>
+
+
+ </div>   
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script>
+	$('.owl-carousel').owlCarousel({
+	autoWidth:true,
+	dots:false,
+    loop:true,
+    margin:10,
+    nav:true,
+	navText : ['<i class="fa fa-angle-left" aria-hidden="true"><img style="width="15px" height="45px"" src="Nav_left.png"></i>','<i class="fa fa-angle-right" aria-hidden="true"><img style="width="10px" height="45px"" src="Nav_right.png"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:6
+        },
+        1000:{
+            items:9
+        }
+    }
+})
+</script>
+</html>
